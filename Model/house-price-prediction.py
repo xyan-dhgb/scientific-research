c9d2fa5c7950366ecc048ed591032373a0888321
Đinh Huỳnh Gia Bảo <100998772+andreafletcherdinh@users.noreply.github.com>
2024-11-08 11:10:08 +0700

    Update house-price-prediction.py

--- Model/house-price-prediction.py
+++ Model/house-price-prediction.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 
-
-# In[3]:
-
-
 house_data = pd.read_csv('D:\\RESEARCH\\Dataset\\melbourne-housing-snapshot.csv')
-
-
-# In[19]:
-
 
 # Hiển thị toàn bộ bảng dữ liệu (không giới hạn số dòng, số cột)
 pd.set_option('display.max_rows', None)
@@ -22,48 +11,17 @@ pd.set_option('display.max_columns', None)
 
 print(house_data)
 
-
-# In[4]:
-
-
 print(house_data.columns)
-
-
-# In[5]:
-
-
-# Drop missing values
 
 house_data = house_data.dropna(axis=0)
 
-
-# In[12]:
-
-
 y = house_data.Price
-
-
-# In[8]:
-
 
 house_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 
-
-# In[ ]:
-
-
-# X: Convention 
 x = house_data[house_features]
 
-
-# In[11]:
-
-
 x.describe()
-
-
-# In[14]:
-
 
 from sklearn.tree import DecisionTreeRegressor
 
@@ -71,10 +29,6 @@ from sklearn.tree import DecisionTreeRegressor
 house_data_model = DecisionTreeRegressor(random_state=1)
 
 house_data_model.fit(x,y)
-
-
-# In[15]:
-
 
 print("Making predictions for the following 5 houses:")
 print(x.head())
